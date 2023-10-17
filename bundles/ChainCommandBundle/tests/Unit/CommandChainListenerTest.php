0afb07b19854f613686e8a1db918e8910c59cd44
vasyldmutryk <dmutrykv@gmail.com>
2023-10-17 10:25:09 +0300

    remove not used classes

--- bundles/ChainCommandBundle/tests/Unit/CommandChainListenerTest.php
+++ bundles/ChainCommandBundle/tests/Unit/CommandChainListenerTest.php
@@ -7,14 +7,10 @@ use ChainCommandBundle\CommandChainRegistry;
 use ChainCommandBundle\Exception\WrongChainCommandCallException;
 use Monolog\Logger;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class CommandChainListenerTest extends TestCase
 {
